a963d294a336386b386b23cc8f83ee5a3f7d51c0
nielsVoogt <niels@branchonline.nl>
2017-08-11 16:05:44 +0200

    Added autoprefixr just in case..

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,17 @@
 
 var gulp         = require('gulp'),
     watch        = require('gulp-watch'),
+    autoprefixer = require('gulp-autoprefixer'),
     sass         = require('gulp-sass');
 
 function onError(err) {
     console.log(err);
     this.emit('end');
 }
+
+var autoprefixerOptions = {
+    browsers: ['last 2 versions', '> 1%', 'Safari 8', 'Firefox ESR']
+};
 
 var sassOptions = {
     errLogToConsole: true,
@@ -22,5 +27,6 @@ gulp.task('watch', ['sass'], function() {
 gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(autoprefixer(autoprefixerOptions))
     .pipe(gulp.dest('./src/css/'));
 });
